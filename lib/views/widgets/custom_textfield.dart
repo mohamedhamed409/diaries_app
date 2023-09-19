c42243c4accfb0d768bf27c881c9c21036a346d4
mohamedhamed409 <mohamedhamed.mu@gmail.com>
2023-09-19 12:36:07 +0300

    add_textfield to add note bottom sheet

--- lib/views/widgets/custom_textfield.dart
+++ lib/views/widgets/custom_textfield.dart
@@ -2,14 +2,17 @@ import 'package:diaries_app/constants.dart';
 import 'package:flutter/material.dart';
 
 class CustomTextField extends StatelessWidget {
-  const CustomTextField({super.key});
-
+  const CustomTextField({super.key,this.hint,this.maxLines=1});
+  final String? hint;
+  final int? maxLines;
   @override
   Widget build(BuildContext context) {
     return TextField(
       cursorColor: kPrimaryColor,
+      maxLines: maxLines,
       decoration: InputDecoration(
-          hintText: 'Title',
+          hintText: hint,
+          
           border: customBorder(),
           enabledBorder: customBorder(),
           focusedBorder: customBorder(kPrimaryColor)),
