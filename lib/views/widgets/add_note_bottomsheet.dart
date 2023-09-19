e64e10f7ad96da318c849be48f31a5c1f0cced8f
mohamedhamed409 <mohamedhamed.mu@gmail.com>
2023-09-19 12:18:51 +0300

    custom textfield

--- lib/views/widgets/add_note_bottomsheet.dart
+++ lib/views/widgets/add_note_bottomsheet.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'custom_textfield.dart';
+
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(),
-        child: const Column(
-          children: [
-            Text(''),
-          ],
-        ));
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child: Column(
+        children: [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextField(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/views/widgets/custom_textfield.dart
@@ -0,0 +1,25 @@
+import 'package:diaries_app/constants.dart';
+import 'package:flutter/material.dart';
+
+class CustomTextField extends StatelessWidget {
+  const CustomTextField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+          hintText: 'Title',
+          border: customBorder(),
+          enabledBorder: customBorder(),
+          focusedBorder: customBorder(kPrimaryColor)),
+    );
+  }
+
+  OutlineInputBorder customBorder([color]) {
+    return OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: color ?? Colors.white),
+    );
+  }
+}
